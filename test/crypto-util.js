723d45573c923bdacf05b8dc44759efa27a8ec49
Pascal <pascal.gruen@gmail.com>
2020-04-29 21:26:53 +0200

    Update dependencies

--- test/crypto-util.js
+++ test/crypto-util.js
@@ -7,20 +7,18 @@ const branca = require("branca");
 function setupCrypto() {
   // Fernet:
   const setIV = fernet.Token.prototype.setIV;
-  fernet.Token.prototype.setIV = function() {
+  fernet.Token.prototype.setIV = function () {
     const iv = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
     setIV.apply(this, [iv]);
   };
   const setTime = fernet.Token.prototype.setTime;
-  fernet.Token.prototype.setTime = function() {
+  fernet.Token.prototype.setTime = function () {
     setTime.apply(this, [1000]);
   };
 }
 
 function decryptBranca(key, msg) {
-  return branca(key)
-    .decode(msg)
-    .toString();
+  return branca(key).decode(msg).toString();
 }
 
 module.exports = { setupCrypto, decryptBranca };
