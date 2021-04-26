7e48081968f41556f10c696db9e267e050b75a28
Pascal <pascal.gruen@gmail.com>
2021-04-26 21:50:37 +0200

    Support quotes in path

--- test/utils-test.js
+++ test/utils-test.js
@@ -10,4 +10,29 @@ describe("utils", () => {
   it("should throw an error when no key is given", () => {
     expect(() => utils.tryDecrypt()).to.throw(/no decryption keys given/);
   });
+
+  it("should split the path", () => {
+    expect(utils.splitPath("a.b")).to.deep.equal(["a", "b"]);
+    expect(utils.splitPath("a.b.c")).to.deep.equal(["a", "b", "c"]);
+    expect(utils.splitPath("a.'b'")).to.deep.equal(["a", "b"]);
+    expect(utils.splitPath('"a".b')).to.deep.equal(["a", "b"]);
+    expect(utils.splitPath('"a.b".c')).to.deep.equal(["a.b", "c"]);
+    expect(utils.splitPath("ab.cd[ef].gh")).to.deep.equal([
+      "ab",
+      "cd",
+      "ef",
+      "gh"
+    ]);
+    expect(utils.splitPath("a.b.'a.b'[a..s][a..]")).to.deep.equal([
+      "a",
+      "b",
+      "a.b",
+      "a..s",
+      "a.."
+    ]);
+  });
+
+  it("should return an error when splitting an invalid path", () => {
+    expect(() => utils.splitPath("a.b[")).to.throw(/unmatched separator/);
+  });
 });
